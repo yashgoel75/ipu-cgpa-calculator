ff0bd11f41784b4be48a8ebb1c6c1624dd765f15
yashgoel <yash.goel8370@gmail.com>
2025-01-10 16:04:52 +0530

    update html, css, js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const parentElement = document.getElementById('subjects');
+const parentElementSGPA = document.getElementById('subjects');
+const parentElementCGPA = document.getElementById('sems');
 let numberOfSub;
+let numberOfSem;
+
 subjectCount.addEventListener('change', () => {
     const numSubject = document.getElementById('subjectCount').value;
     numberOfSub = numSubject;
-    parentElement.innerHTML = '';
+    parentElementSGPA.innerHTML = '';
 
     for (let i = 0; i < numSubject; i++) {
         const subjectDiv = document.createElement('div');
@@ -44,9 +47,79 @@ subjectCount.addEventListener('change', () => {
 
         subjectDiv.appendChild(inputGroup);
 
-        parentElement.appendChild(subjectDiv);
+        parentElementSGPA.appendChild(subjectDiv);
     }
 });
+
+semCount.addEventListener('change', () => {
+    const numSem = document.getElementById('semCount').value;
+    numberOfSem = numSem;
+    parentElementCGPA.innerHTML = '';
+
+    for (let i = 0; i < numSem; i++) {
+        const semDiv = document.createElement('div');
+        semDiv.classList.add('input-group', 'mb-3');
+
+        const inputGroup = document.createElement('div');
+        inputGroup.classList.add('input-group', 'mb-3');
+
+        const sgpaSpan = document.createElement('span');
+        sgpaSpan.classList.add("input-group-text");
+        sgpaSpan.textContent = `SGPA in Semester ${i + 1}`;
+        inputGroup.appendChild(sgpaSpan);
+
+        const inputMarks = document.createElement('input');
+        inputMarks.type = "number";
+        inputMarks.required = true;
+        inputMarks.min = 0;
+        inputMarks.max = 10;
+        inputMarks.required = true;
+        inputMarks.id = `sgpa${i + 1}`;
+        inputMarks.classList.add("form-control");
+        inputGroup.appendChild(inputMarks);
+
+        const creditSpan = document.createElement('span');
+        creditSpan.classList.add("input-group-text");
+        creditSpan.textContent = `Total Credits in Semester ${i + 1}`;
+        inputGroup.appendChild(creditSpan);
+
+        const inputCredit = document.createElement('input');
+        inputCredit.type = "number";
+        inputCredit.required = true;
+        inputCredit.min = 0;
+        inputCredit.max = 100;
+        inputCredit.required = true;
+        inputCredit.id = `creditCGPA${i + 1}`;
+        inputCredit.classList.add("form-control");
+        inputGroup.appendChild(inputCredit);
+
+        semDiv.appendChild(inputGroup);
+
+        parentElementCGPA.appendChild(semDiv);
+    }
+});
+
+function sgpa() {
+    const sgpaSelection = document.getElementsByClassName('sgpaCalculation');
+    const cgpaSelection = document.getElementsByClassName('cgpaCalculation');
+    for (const element of sgpaSelection) {
+        element.style.display = 'block';
+    }
+    for (const element of cgpaSelection) {
+        element.style.display = 'none';
+    }
+}
+
+function cgpa() {
+    const sgpaSelection = document.getElementsByClassName('sgpaCalculation');
+    const cgpaSelection = document.getElementsByClassName('cgpaCalculation');
+    for (const element of sgpaSelection) {
+        element.style.display = 'none';
+    }
+    for (const element of cgpaSelection) {
+        element.style.display = 'block';
+    }
+}
 
 function handleClear() {
     result.innerHTML = '-';
@@ -91,4 +164,41 @@ function handleSubmit() {
     result.innerHTML = '';
     result.innerHTML = Math.round((gradePoint / creditSum) * 100) / 100;
     result.textContent = Math.round((gradePoint / creditSum) * 100) / 100;
+}
+
+function handleClearCGPA() {
+    resultCGPA.innerHTML = '-';
+    resultCGPA.textContent = '-';
+}
+function handleSubmitCGPA() {
+    let SGPAsum = 0;
+    let creditSum = 0;
+
+    for (let i = 0; i < numberOfSem; i++) {
+        const currentSGPA = parseInt(document.getElementById(`sgpa${i + 1}`).value);
+        const currentCredit = parseInt(document.getElementById(`creditCGPA${i + 1}`).value);
+
+        if (isNaN(currentSGPA) || isNaN(currentCredit)) {
+            alert(`Please fill in all fields for Subject ${i + 1}.`);
+            marksInput.focus();
+            return;
+        }
+        if (currentSGPA > 10) {
+            alert(`Maximum SGPA for a semester cannot exceed 10. Please check Semester ${i + 1}.`);
+            return;
+        }
+        if (currentCredit > 52) {
+            alert(`Maximum credits for a semester cannot exceed 52. Please check Semester ${i + 1}.`);
+            return;
+        }
+        const productOfSGPACredit = currentSGPA * currentCredit;
+        SGPAsum += productOfSGPACredit;
+        creditSum += currentCredit;
+    }
+
+    const result = document.getElementById('resultCGPA');
+    result.innerHTML = '';
+    result.innerHTML = Math.round((SGPAsum / creditSum) * 100) / 100;
+    result.textContent = Math.round((SGPAsum / creditSum) * 100) / 100;
+    console.log(result);
 }
